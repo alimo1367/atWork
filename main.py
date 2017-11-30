2ab097ecd44524ec1d3afa4e2844ec14cb63b6e9
alimo1367 <alimo.1367@gmail.com>
2017-11-30 10:22:39 +0330

    add imports and server classes

---
+++ main.py
@@ -0,0 +1,50 @@
+﻿
+#from geometry_msgs.msg import Twist
+import numpy as np
+import glob
+import pickle
+
+import logging
+import time
+import cv2
+import pyrealsense as pyrs
+import msgpackrpc
+#from pyrealsense import rsutilwrapper
+#from ipdb3 import set_trace
+
+
+
+#print(cv2.__version__)
+
+
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+#from matplotlib import pyplot as plt
+from constants import rs_option
+#from blendfunc import blend
+
+#------------------------------------------------------
+#----------        Threading for send data ------------
+import Pyro4
+import thread
+
+
+@Pyro4.expose
+
+class GreetingMaker2(object):
+    def get_fortune(self, name):
+        global Idx, Dx,Dy,Dp,Orint,Width,Hight
+        if(name==0):
+          Dx, Dy, Dp, Orint ,Width,Hight= 0,0,0,0,0,0
+        return Idx,Dx,Dy,Dp,Orint,str(Width),str(Hight)
+
+class SumServer(object):
+    def get_fortune(self, name):
+        global Idx, Dx,Dy,Dp,Orint,Width,Hight
+        if(name==0):
+          Dx, Dy, Dp, Orint ,Width,Hight= 0,0,0,0,0,0
+        return Idx,Dx,Dy,Dp,Orint,str(Width),str(Hight)
+
+
+
+def run1(SS):
