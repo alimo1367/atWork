4b7c6bb525063f0d24015803b74600d650817271
alimo1367 <alimo.1367@gmail.com>
2017-12-01 13:47:12 +0330

    start server thread and daemon

--- main.py
+++ main.py
@@ -48,3 +48,28 @@ class SumServer(object):
 
 
 def run1(SS):
+    global daemon
+    # while True:
+    print("Ready.")
+    time.sleep(SS)
+    daemon.requestLoop()  # start the event loop of the server to wait for calls
+
+
+
+
+
+
+
+
+
+#-----------------------------
+SS=1
+thread.start_new_thread(run1, (SS,))
+
+daemon = Pyro4.Daemon()  # make a Pyro daemon
+ns = Pyro4.locateNS()  # find the name server
+uri = daemon.register(GreetingMaker2)  # register the greeting maker as a Pyro object
+ns.register("ali1", uri)  # register the object with a name in the name server
+#------------------------------------------------------
+
+#===========================================================================
