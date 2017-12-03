af36a4a478aa7315f35bdf75f236abc96cbe5266
alimo1367 <alimo.1367@gmail.com>
2017-12-03 09:18:44 +0330

    add vision system variables

--- main.py
+++ main.py
@@ -73,3 +73,65 @@ ns.register("ali1", uri)  # register the object with a name in the name server
 #------------------------------------------------------
 
 #===========================================================================
+#----------------------   Defining Global variables od vision system -------
+
+#print(myRobot.PoseAr)
+logging.basicConfig(level=logging.INFO)
+shapeMask =0
+last = time.time()
+smoothing = 0.9
+DepthScale = 125
+fps_smooth = 60
+ObjectsGridNumber = 20
+ObjectsSamples  = 7
+ObjectsOrientationInGrid = np.zeros((ObjectsSamples,ObjectsGridNumber,ObjectsGridNumber),dtype='int16')
+ObjectsLabelInGrid       = np.zeros((ObjectsSamples,ObjectsGridNumber,ObjectsGridNumber),dtype='int16')
+kernel2=np.array([[1,  1,  1, 1, 1],
+                  [1,  1, -2, 1, 1],
+                  [1, -2,-12,-2, 1],
+                  [1,  1, -2, 1, 1],
+                  [1,  1,  1, 1, 1]
+                 ])
+kernel1=np.array([[1,  1, 1],
+                 [1,  -5, 1],
+                 [1,  1, 1]
+                 ])
+
+ObjectsLabel= { 0000: ' -------   ',
+                1020: 'Bk SmProfl ',
+                1021: 'Wh SmProfl ',
+                1040: 'Bk BgProfl ',
+                1041: 'Wh BgProfl ',
+                1120: 'Screw      ',
+                1220: 'Sm Nut     ',
+                1230: 'Bg Nut     ',
+                1320: 'PlasticTube',
+                1410: 'Bearing Box',
+                1510: 'Bearing    ',
+                1610: 'Axis       ',
+                1710: 'Dist  Tube ',
+                1810: 'Motor      ',
+                9999: 'Nothings   ' }
+
+ImageSize             = (640  , 480)
+ImageCenter           = (ImageSize[0]/2, ImageSize[1]/2)
+Dthreshold            = 120
+MinArea               = 100
+MaxArea               = 50000
+Wcrop                 = 15
+kernel                = np.ones((5, 5), np.float32) / 15
+surf                  = cv2.SURF(85)
+surf.hessianThreshold =  1150
+surf.extended         = 0     # 0 for 64 Bins , 1 for 128 Bins
+BoWSize               = 100
+svm_params            = dict(kernel_type = cv2.SVM_RBF, svm_type = cv2.SVM_C_SVC, C=4.67, gamma=1.383)
+DepthSamples          = 4
+DDepths = np.zeros(shape=(ImageSize[1],ImageSize[0]) + (DepthSamples+1,))
+ImgCADs = np.zeros(shape=(ImageSize[1],ImageSize[0]) + (DepthSamples+1,))
+DepthM  =0
+#===========================================================================
+
+#===========================================================================
+#------------------- Defining basic functins -------------------------------
+#        These functions are calls in intialization and in the main loop
+#---------------------------------------------------------------------------
